5fb90cb2ae743363be58a9bd64a98de4b8f92ed8
Parkjimin <parkjimin1016@gmail.com>
2021-03-29 19:32:39 +0900

    4주차

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,7 @@ int main(int argc, const char** argv) {
 
         // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
-    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
-      nullptr, nullptr);
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
     if (!window) {
         SPDLOG_ERROR("failed to create glfw window");
         glfwTerminate();
